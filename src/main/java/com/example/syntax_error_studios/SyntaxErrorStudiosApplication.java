d104f62076ec061ecd604966734e0e60385836a5
Daniel <daniel.pearce.exe@gmail.com>
2026-05-14 21:18:13 -0400

    first commit

---
+++ src/main/java/com/example/syntax_error_studios/SyntaxErrorStudiosApplication.java
@@ -0,0 +1,13 @@
+package com.example.syntax_error_studios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntaxErrorStudiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntaxErrorStudiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/syntax_error_studios/SyntaxErrorStudiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syntax_error_studios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntaxErrorStudiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

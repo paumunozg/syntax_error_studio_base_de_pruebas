45df092d5caee820b15090f8ca03c77d2a648152
Paula Muñoz González <paula.munozg@duocuc.cl>
2026-05-24 17:58:02 -0400

    Creación de packages y clases para Service y DTO
    Se crean los packages Service y DTO y se crean en su interior las clases:
    ModalidadService
    ConsolaService
    GeneroService
    VideojuegoService
    
    y las clases
    VideojuegoRequestDTO
    VideojuegoResponseDTO

---
+++ src/main/java/com/example/syntax_error_studios/dto/VideojuegoRequestDTO.java
@@ -0,0 +1,5 @@
+package com.example.syntax_error_studios.dto;
+
+public class VideojuegoRequestDTO {
+
+}
---
+++ src/main/java/com/example/syntax_error_studios/service/ConsolaService.java
@@ -0,0 +1,5 @@
+package com.example.syntax_error_studios.service;
+
+public class ConsolaService {
+
+}

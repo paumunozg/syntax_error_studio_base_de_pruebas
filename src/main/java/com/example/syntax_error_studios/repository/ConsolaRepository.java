adc017c1e9719045c49c96a09d385568580ce69e
Paula Muñoz González <paula.munozg@duocuc.cl>
2026-05-24 18:52:27 -0400

    actualización repositorio consola
    se hizo una pequeña modificación en el código, ya no es necesario declarar @Repository

--- src/main/java/com/example/syntax_error_studios/repository/ConsolaRepository.java
+++ src/main/java/com/example/syntax_error_studios/repository/ConsolaRepository.java
@@ -1,10 +1,9 @@
 package com.example.syntax_error_studios.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.syntax_error_studios.model.Consola;
-@Repository
+
 public interface ConsolaRepository extends JpaRepository<Consola,Long> {
 
 }

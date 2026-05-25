3f147ceebebef1381f9dc866e7dafa59230e87d2
Paula Muñoz González <paula.munozg@duocuc.cl>
2026-05-24 22:48:01 -0400

    Actualización VideojuegoController
    Se le agregan los métodos get para acceder a los microservicios extra creados para el proyecto.

--- src/main/java/com/example/syntax_error_studios/controller/VideojuegoController.java
+++ src/main/java/com/example/syntax_error_studios/controller/VideojuegoController.java
@@ -55,4 +55,58 @@ public class VideojuegoController {
         videojuegoService.eliminar(id);
         return ResponseEntity.noContent().build();
     }
+
+    //Buscar por nombre de videojuego
+    @GetMapping("/nombre")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorNombre(@RequestParam String nombre) {
+        return ResponseEntity.ok(videojuegoService.buscarPorNombre(nombre));
+    }
+
+    //Buscar por género de videojuego
+    @GetMapping("/genero")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorGenero(@RequestParam String genero) {
+        return ResponseEntity.ok(videojuegoService.buscarPorGenero(genero));
+    }
+
+    //Buscar por consola
+    @GetMapping("/consola")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorConsola(@RequestParam String consola) {
+        return ResponseEntity.ok(videojuegoService.buscarPorConsola(consola));
+    }
+
+    //Buscar por precio
+    @GetMapping("/precio")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorPrecio(@RequestParam int precio) {
+        return ResponseEntity.ok(videojuegoService.buscarPorPrecio(precio));
+    }
+
+    //Buscar por modalidad
+    @GetMapping("/modalidad")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorModalidad(@RequestParam String modalidad) {
+        return ResponseEntity.ok(videojuegoService.buscarPorModalidad(modalidad));
+    }
+
+    //Buscar por estado
+    @GetMapping("/estado")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorEstado(@RequestParam String estado) {
+        return ResponseEntity.ok(videojuegoService.buscarPorEstado(estado));
+    }
+
+    //Buscar por EAN
+    @GetMapping("/ean")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorEAN(@RequestParam String ean) {
+        return ResponseEntity.ok(videojuegoService.buscarPorEAN(ean));
+    }
+
+    //Buscar por precio menor o igual
+    @GetMapping("/precio-menor")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorPrecioMenor(@RequestParam int precio) {
+        return ResponseEntity.ok(videojuegoService.buscarPorPrecioMenor(precio));
+    }
+
+    //Buscar por precio mayor o igual
+    @GetMapping("/precio-mayor")
+    public ResponseEntity<List<VideojuegoResponseDTO>> buscarPorPrecioMayor(@RequestParam int precio) {
+        return ResponseEntity.ok(videojuegoService.buscarPorPrecioMayor(precio));
+    }
 }
